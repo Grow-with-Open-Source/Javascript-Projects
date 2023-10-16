bbe7c1c2857cc6a151dfc24f00b39429fd93a022
Shamith <shamith301102@gmail.com>
2023-10-16 13:14:47 +0530

    Update convert_to_html_tables.py

--- .github/scripts/convert_to_html_tables.py
+++ .github/scripts/convert_to_html_tables.py
@@ -92,7 +92,7 @@ def main():
 		if ' ' in demo_path:
 			demo_path = '%20'.join(demo_path.split())
 		demo_path_output = f'<a href="{demo_path}" title="view the result of {title}">/{REPO_NAME}/{title}/</a>'
-		if demo_path == 'root' or demo_path == '{init}':
+		if title == 'root' or title == '{init}':
 			demo_path_output = f'<a href="{demo_path}" title="view the result of {title}">/{REPO_NAME}/</a>'
 
 		# Appending all data together
